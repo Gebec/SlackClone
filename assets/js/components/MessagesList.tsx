f932e7e012fe787f1f70baf7b8c4643221dac78c
Martin Gebauer <gebec.m@gmail.com>
2018-09-21 12:56:59 +0200

    Fixed messages scroll problem

--- assets/js/components/MessagesList.tsx
+++ assets/js/components/MessagesList.tsx
@@ -22,10 +22,129 @@ export default class MessageList extends React.Component <{}> {
         name: 'Author 4',
         text: 'Text message 4'
       },
+
+      {
+        name: 'Author 1',
+        text: 'Text message 1'
+      },
+      {
+        name: 'Author 2',
+        text: 'Text message 2'
+      },
+      {
+        name: 'Author 3',
+        text: 'Text message 3'
+      },
+      {
+        name: 'Author 4',
+        text: 'Text message 4'
+      },
+
+      {
+        name: 'Author 1',
+        text: 'Text message 1'
+      },
+      {
+        name: 'Author 2',
+        text: 'Text message 2'
+      },
+      {
+        name: 'Author 3',
+        text: 'Text message 3'
+      },
+      {
+        name: 'Author 4',
+        text: 'Text message 4'
+      },
+
+      {
+        name: 'Author 1',
+        text: 'Text message 1'
+      },
+      {
+        name: 'Author 2',
+        text: 'Text message 2'
+      },
+      {
+        name: 'Author 3',
+        text: 'Text message 3'
+      },
+      {
+        name: 'Author 4',
+        text: 'Text message 4'
+      },
+
+      {
+        name: 'Author 1',
+        text: 'Text message 1'
+      },
+      {
+        name: 'Author 2',
+        text: 'Text message 2'
+      },
+      {
+        name: 'Author 3',
+        text: 'Text message 3'
+      },
+      {
+        name: 'Author 4',
+        text: 'Text message 4'
+      },
+
+      {
+        name: 'Author 1',
+        text: 'Text message 1'
+      },
+      {
+        name: 'Author 2',
+        text: 'Text message 2'
+      },
+      {
+        name: 'Author 3',
+        text: 'Text message 3'
+      },
+      {
+        name: 'Author 4',
+        text: 'Text message 4'
+      },
+
+      {
+        name: 'Author 1',
+        text: 'Text message 1'
+      },
+      {
+        name: 'Author 2',
+        text: 'Text message 2'
+      },
+      {
+        name: 'Author 3',
+        text: 'Text message 3'
+      },
+      {
+        name: 'Author 4',
+        text: 'Text message 4'
+      },
+
+      {
+        name: 'Author 1',
+        text: 'Text message 1'
+      },
+      {
+        name: 'Author 2',
+        text: 'Text message 2'
+      },
+      {
+        name: 'Author 3',
+        text: 'Text message 3'
+      },
+      {
+        name: 'Author 4',
+        text: 'Text message 4'
+      },
     ];
 
     return (
-      <div className="flex flex--col jc-end h-full">
+      <div className="flex flex--col jc-end h-full scroll">
         {DUMMY_MESSAGES.map((message, i) => <Message name={message.name} text={message.text} key={i}></Message>)}
       </div>
     );
