f5fb4996e1b7116c4fe8ae2198f25ceebf2818b1
Martin Gebauer <gebec.m@gmail.com>
2018-09-25 12:29:31 +0200

    Added placeholder text when no messages in room

--- assets/js/components/MessagesList.tsx
+++ assets/js/components/MessagesList.tsx
@@ -9,9 +9,20 @@ interface MessageInterface{
 }
 
 const MessageList: React.SFC<{messages: MessageInterface[]}> = (props) => {
+
+  const emptyList = (
+    <div>
+      Be the first to post in this room
+    </div>
+  );
+
   return (
     <div className="flex flex--col jc-end h-full scroll">
-      { props.messages.map((message, i) => <Message username={message.senderId} text={message.text} key={i}></Message>)}
+      { props.messages.length > 0 ? (
+        props.messages.map((message, i) => <Message username={message.senderId} text={message.text} key={i}></Message>)
+      ) : (
+        emptyList
+      )}
     </div>
   );
 }
